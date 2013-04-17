21a4efabe3bc17bf42f2d2f133bb39395ae6ee8c
Mark N. Sikora <marknsikora@gmail.com>
2013-04-17 17:09:50 -0400

    Solution for All Your Base

---
+++ 189252/A.hs
@@ -0,0 +1,22 @@
+import Control.Monad
+import Data.List
+import Data.Maybe
+import Text.Printf
+
+main = do
+  tests <- readLn
+  
+  forM_ [1..tests] $ \ caseNum -> do
+    str <- getLine
+    
+    let digify c = case elemIndex c (nub str) of
+          Just 0  -> 1
+          Just 1  -> 0
+          Just n  -> n
+          Nothing -> error "The impossible happened!"
+    
+    let base = succ . maximum . map digify $ str
+    
+    let ans = foldl (\x y -> x*base + y) 0 . map digify $ str
+    
+    printf "Case #%d: %d\n" (caseNum::Int) (ans::Int)
