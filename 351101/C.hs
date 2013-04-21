367ed5fcc2ddf864674992608c03f429dd74d244
Mark N. Sikora <marknsikora@gmail.com>
2013-04-21 13:04:07 -0400

    Solutions for T9 Spelling

---
+++ 351101/C.hs
@@ -0,0 +1,27 @@
+import Control.Monad
+import Data.Char
+import Data.List
+import Data.Maybe
+import Text.Printf
+
+main = do
+  tests <- readLn
+
+  forM_ [1..tests] $ \caseNum -> do
+    str <- getLine
+
+    let ans = concatMap insertPause . groupBy pauseNeeded $ numified where
+          numified = map numify str
+          pauseNeeded x y = head x == head y
+          insertPause = intercalate " "
+
+    printf "Case #%d: %s\n" (caseNum::Int) ans
+
+numify c = replicate count digit where
+  digit = case findIndex (c `elem`) dict of
+    Just n  -> intToDigit n
+    Nothing -> error "Invalid character"
+  count = case elemIndex c =<< find (c `elem`) dict of
+    Just n  -> n + 1
+    Nothing -> error "Invalid character"
+  dict = [" ","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
