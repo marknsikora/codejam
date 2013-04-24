e82538d4e6de14f62bf19b88ecb42dd73495c8fb
Mark N. Sikora <marknsikora@gmail.com>
2013-04-24 04:31:28 +0200

    Move intermediates into where clause

--- 189252/A.hs
+++ 189252/A.hs
@@ -9,14 +9,12 @@ main = do
   forM_ [1..tests] $ \ caseNum -> do
     str <- getLine
     
-    let digify c = case elemIndex c (nub str) of
-          Just 0  -> 1
-          Just 1  -> 0
-          Just n  -> n
-          Nothing -> error "The impossible happened!"
-    
-    let base = succ . maximum . map digify $ str
-    
-    let ans = foldl (\x y -> x*base + y) 0 . map digify $ str
+    let ans = foldl (\x y -> x*base + y) 0 . map digify $ str where
+          digify c = case elemIndex c (nub str) of
+            Just 0  -> 1
+            Just 1  -> 0
+            Just n  -> n
+            Nothing -> error "The impossible happened!"
+          base = succ . maximum . map digify $ str
     
     printf "Case #%d: %d\n" (caseNum::Int) (ans::Int)
