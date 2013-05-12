71f7b7503c44dbdf1f29b8f92373e78ca9573d0a
Mark N. Sikora <marknsikora@gmail.com>
2013-05-12 13:33:48 +0200

    Add (bad) solutions to round 1C 2013

---
+++ 2437488/A.hs
@@ -0,0 +1,21 @@
+import Control.Applicative
+import Control.Monad
+import Data.Function
+import Data.List
+import Text.Printf
+
+main = do
+  tests <- readLn
+  
+  forM_ [1..tests] $ \caseNum -> do
+    [w,_n] <- words <$> getLine
+    
+    let n = read _n
+
+    let ans = sum . map (succ . length . remaining) . filter valid . tails $ w where
+          remaining = drop n . concat . dropWhile (\z -> any isVowel z || length z < n) . groupBy ((==) `on` isVowel)
+          valid = any (>=n) . map length . filter (not . any isVowel) . groupBy ((==) `on` isVowel)
+
+    printf "Case #%d: %d\n" (caseNum::Int) (ans)
+
+isVowel c = c `elem` "aeiou"
