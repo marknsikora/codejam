2589c84d2fa0b852d292eb0e0f77323f8d3af85f
Mark Sikora <marknsikora@gmail.com>
2014-04-12 18:52:27 -0400

    Use Int instead of double to avoid rounding error

--- 2974486/D.hs
+++ 2974486/D.hs
@@ -13,11 +13,13 @@ main = do
 
   forM_ [1..tests] $ \caseNum -> do
     _ <- getLine
-    naomi <- Set.fromList . map read . words <$> getLine
-    ken <- Set.fromList . map read . words <$> getLine
+    naomi <- Set.fromList . map (read . padBlock) . words <$> getLine
+    ken <- Set.fromList . map (read . padBlock) . words <$> getLine
 
     let optimal = fst $ solve (0,0) naomi ken
-        deceitful = snd $ solve (0,0) ken (naomi::Set.Set Double)
+        deceitful = snd $ solve (0,0) ken naomi
+
+        solve :: (Int,Int) -> Set.Set Int -> Set.Set Int -> (Int,Int)
         solve points x y
           | Set.null x = points
           | Set.null y = points
@@ -29,3 +31,9 @@ main = do
               succPoints l = over l succ points
 
     printf "Case #%d: %d %d\n" (caseNum::Int) (deceitful::Int) (optimal::Int)
+
+padBlock :: String -> String
+padBlock s = go (drop 2 s) [1..5] where
+  go (x:xs) (_:ys) = x : go xs ys
+  go [] (y:ys) = '0' : go [] ys
+  go _ _ = []
