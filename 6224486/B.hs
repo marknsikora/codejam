9ee80deb0f52e10b8320e84ff9b15151d05da249
Mark Sikora <marknsikora@gmail.com>
2015-04-11 18:38:15 -0400

    Reduce all plates at once

--- 6224486/B.hs
+++ 6224486/B.hs
@@ -16,11 +16,14 @@ main = do
     let ans = reduce 0 (sortWith Down (d::[Int]))
           where
             reduce n [] = n
-            reduce n xss@(x:xs)
+            reduce n xss@(x:_)
               | d <= headNum = x + n
-              | otherwise = reduce (succ n) (sortWith Down $ (d+r):d:xs)
+              | otherwise = reduce (n + headNum) (sortWith Down next)
               where
                 (d, r) = divMod x 2
                 headNum = length . head . group $ xss
+                next = (concat . tail . group $ xss) ++
+                       (replicate headNum d) ++
+                       (replicate headNum (d+r))
 
     printf "Case #%d: %d\n" (caseNum::Int) (ans::Int)
