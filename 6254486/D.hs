48ecfe5898486488d55ecb52843e11bf5c513a94
Mark Sikora <marknsikora@gmail.com>
2016-04-10 23:12:37 -0400

    Working solution for large D

--- 6254486/D.hs
+++ 6254486/D.hs
@@ -1,6 +1,7 @@
 import Control.Applicative
 import Control.Monad
 import Data.List
+import Data.List.Split
 import Text.Printf
 
 import qualified Data.Set as Set
@@ -12,11 +13,10 @@ main = do
   forM_ [1..tests] $ \caseNum -> do
     [k,c,s] <- map read . words <$> getLine
 
-    let ans = if s < k then Nothing else Just solve
-          where
-            solve = map (f k c) [1..k]
-
-            f _ 1 z = z
-            f x y z = x^(y-1) * (z-1) + (f x (y-1) z)
+    let ans | length paths > s = Nothing
+            | otherwise = Just $ map followPaths paths
+              where
+                paths = chunksOf c $ [0..k-1]
+                followPaths = succ . sum . zipWith (*) (iterate (*k) 1) . reverse
 
     printf "Case #%d: %s\n" (caseNum::Int) (maybe "IMPOSSIBLE" (unwords . map show) ans)
