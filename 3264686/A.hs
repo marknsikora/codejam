d5890d819fc39dfb134d1ce0d2d90b97f0f3cec7
Mark Sikora <marknsikora@gmail.com>
2017-04-08 18:45:19 -0400

    Cleanup solution

--- 3264686/A.hs
+++ 3264686/A.hs
@@ -3,11 +3,6 @@ import Control.Monad
 import Data.List
 import Text.Printf
 
-import qualified Data.Char as Char
-
-data Pancake = Happy | Blank
-  deriving (Eq, Show)
-
 main :: IO ()
 main = do
   tests <- readLn
@@ -15,18 +10,16 @@ main = do
   forM_ [1..tests] $ \caseNum -> do
     [s, k] <- words <$> getLine
 
-    k' <- readIO k :: IO Int
+    k' <- readIO k
 
     let ans = solve s' where
-          s' = map (\c -> if c=='+' then Happy else Blank) s
-          inv Happy = Blank
-          inv Blank = Happy
+          s' = map (=='+') s
 
           solve [] = Just 0
-          solve xs | length xs < k' && not (all (==Happy) xs) = Nothing
-          solve xs = case span (==Happy) xs of
+          solve xs | length xs < k' && not (all id xs) = Nothing
+          solve xs = case span id xs of
                        ([], _) -> let (ys, yss) = splitAt k' xs
-                                   in succ <$> solve (map inv ys ++ yss)
+                                   in succ <$> solve (map not ys ++ yss)
                        (_, ys) -> solve ys
 
     printf "Case #%d: %s\n" (caseNum::Int) (maybe "IMPOSSIBLE" show ans)
